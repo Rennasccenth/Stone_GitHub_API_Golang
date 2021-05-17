c3853ea307d399e34ed9b9e8ee6b6eb778195444
Felipe Nunes <rennasccenth@gmail.com>
2021-05-17 03:03:18 -0400

    feat: Encapsula a camada das requests e tratativas de dados para fora da controller

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,17 +1,29 @@
 package controllers
 
 import (
+	"Stone_GitHub_API_Golang/cmd"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 func GetUserMostStarredRepo(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	user := params["user"]
-	_, err := w.Write([]byte(user))
+	userParam := params["user"]
+
+	//gitHubUser := cmd.GetGitHubUser(userParam)
+	gitHubUser := cmd.User{Login: userParam}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Date", time.Now().String())
+
+	marshalUser, _ := json.Marshal(gitHubUser)
+	_, err := w.Write(marshalUser)
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Print(err)
 	}
 }
 
