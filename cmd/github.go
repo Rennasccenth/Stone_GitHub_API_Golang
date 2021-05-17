432c809daf81140f12e5efb67538b16d22a0bc2a
Felipe Nunes <rennasccenth@gmail.com>
2021-05-17 03:01:49 -0400

    feat: Componentiza as requests ao GitHub e cria as structs User e Repository

--- cmd/github.go
+++ cmd/github.go
@@ -2,16 +2,226 @@ package cmd
 
 import (
 	"Stone_GitHub_API_Golang/env"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-// GenerateAuthenticationHeader generates the header used to authorize
+type User struct {
+	Login                   string      `json:"login"`
+	Id                      int         `json:"id"`
+	NodeId                  string      `json:"node_id"`
+	AvatarUrl               string      `json:"avatar_url"`
+	GravatarId              string      `json:"gravatar_id"`
+	Url                     string      `json:"url"`
+	HtmlUrl                 string      `json:"html_url"`
+	FollowersUrl            string      `json:"followers_url"`
+	FollowingUrl            string      `json:"following_url"`
+	GistsUrl                string      `json:"gists_url"`
+	StarredUrl              string      `json:"starred_url"`
+	SubscriptionsUrl        string      `json:"subscriptions_url"`
+	OrganizationsUrl        string      `json:"organizations_url"`
+	ReposUrl                string      `json:"repos_url"`
+	EventsUrl               string      `json:"events_url"`
+	ReceivedEventsUrl       string      `json:"received_events_url"`
+	Type                    string      `json:"type"`
+	SiteAdmin               bool        `json:"site_admin"`
+	Name                    string      `json:"name"`
+	Company                 interface{} `json:"company"`
+	Blog                    string      `json:"blog"`
+	Location                interface{} `json:"location"`
+	Email                   interface{} `json:"email"`
+	Hireable                interface{} `json:"hireable"`
+	Bio                     string      `json:"bio"`
+	TwitterUsername         interface{} `json:"twitter_username"`
+	PublicRepos             int         `json:"public_repos"`
+	PublicGists             int         `json:"public_gists"`
+	Followers               int         `json:"followers"`
+	Following               int         `json:"following"`
+	CreatedAt               time.Time   `json:"created_at"`
+	UpdatedAt               time.Time   `json:"updated_at"`
+	PrivateGists            int         `json:"private_gists"`
+	TotalPrivateRepos       int         `json:"total_private_repos"`
+	OwnedPrivateRepos       int         `json:"owned_private_repos"`
+	DiskUsage               int         `json:"disk_usage"`
+	Collaborators           int         `json:"collaborators"`
+	TwoFactorAuthentication bool        `json:"two_factor_authentication"`
+	Plan                    struct {
+		Name          string `json:"name"`
+		Space         int    `json:"space"`
+		Collaborators int    `json:"collaborators"`
+		PrivateRepos  int    `json:"private_repos"`
+	} `json:"plan"`
+}
+
+type Repository struct {
+	Id       int    `json:"id"`
+	NodeId   string `json:"node_id"`
+	Name     string `json:"name"`
+	FullName string `json:"full_name"`
+	Private  bool   `json:"private"`
+	Owner    struct {
+		Login             string `json:"login"`
+		Id                int    `json:"id"`
+		NodeId            string `json:"node_id"`
+		AvatarUrl         string `json:"avatar_url"`
+		GravatarId        string `json:"gravatar_id"`
+		Url               string `json:"url"`
+		HtmlUrl           string `json:"html_url"`
+		FollowersUrl      string `json:"followers_url"`
+		FollowingUrl      string `json:"following_url"`
+		GistsUrl          string `json:"gists_url"`
+		StarredUrl        string `json:"starred_url"`
+		SubscriptionsUrl  string `json:"subscriptions_url"`
+		OrganizationsUrl  string `json:"organizations_url"`
+		ReposUrl          string `json:"repos_url"`
+		EventsUrl         string `json:"events_url"`
+		ReceivedEventsUrl string `json:"received_events_url"`
+		Type              string `json:"type"`
+		SiteAdmin         bool   `json:"site_admin"`
+	} `json:"owner"`
+	HtmlUrl          string      `json:"html_url"`
+	Description      *string     `json:"description"`
+	Fork             bool        `json:"fork"`
+	Url              string      `json:"url"`
+	ForksUrl         string      `json:"forks_url"`
+	KeysUrl          string      `json:"keys_url"`
+	CollaboratorsUrl string      `json:"collaborators_url"`
+	TeamsUrl         string      `json:"teams_url"`
+	HooksUrl         string      `json:"hooks_url"`
+	IssueEventsUrl   string      `json:"issue_events_url"`
+	EventsUrl        string      `json:"events_url"`
+	AssigneesUrl     string      `json:"assignees_url"`
+	BranchesUrl      string      `json:"branches_url"`
+	TagsUrl          string      `json:"tags_url"`
+	BlobsUrl         string      `json:"blobs_url"`
+	GitTagsUrl       string      `json:"git_tags_url"`
+	GitRefsUrl       string      `json:"git_refs_url"`
+	TreesUrl         string      `json:"trees_url"`
+	StatusesUrl      string      `json:"statuses_url"`
+	LanguagesUrl     string      `json:"languages_url"`
+	StargazersUrl    string      `json:"stargazers_url"`
+	ContributorsUrl  string      `json:"contributors_url"`
+	SubscribersUrl   string      `json:"subscribers_url"`
+	SubscriptionUrl  string      `json:"subscription_url"`
+	CommitsUrl       string      `json:"commits_url"`
+	GitCommitsUrl    string      `json:"git_commits_url"`
+	CommentsUrl      string      `json:"comments_url"`
+	IssueCommentUrl  string      `json:"issue_comment_url"`
+	ContentsUrl      string      `json:"contents_url"`
+	CompareUrl       string      `json:"compare_url"`
+	MergesUrl        string      `json:"merges_url"`
+	ArchiveUrl       string      `json:"archive_url"`
+	DownloadsUrl     string      `json:"downloads_url"`
+	IssuesUrl        string      `json:"issues_url"`
+	PullsUrl         string      `json:"pulls_url"`
+	MilestonesUrl    string      `json:"milestones_url"`
+	NotificationsUrl string      `json:"notifications_url"`
+	LabelsUrl        string      `json:"labels_url"`
+	ReleasesUrl      string      `json:"releases_url"`
+	DeploymentsUrl   string      `json:"deployments_url"`
+	CreatedAt        time.Time   `json:"created_at"`
+	UpdatedAt        time.Time   `json:"updated_at"`
+	PushedAt         time.Time   `json:"pushed_at"`
+	GitUrl           string      `json:"git_url"`
+	SshUrl           string      `json:"ssh_url"`
+	CloneUrl         string      `json:"clone_url"`
+	SvnUrl           string      `json:"svn_url"`
+	Homepage         interface{} `json:"homepage"`
+	Size             int         `json:"size"`
+	StargazersCount  int         `json:"stargazers_count"`
+	WatchersCount    int         `json:"watchers_count"`
+	Language         string      `json:"language"`
+	HasIssues        bool        `json:"has_issues"`
+	HasProjects      bool        `json:"has_projects"`
+	HasDownloads     bool        `json:"has_downloads"`
+	HasWiki          bool        `json:"has_wiki"`
+	HasPages         bool        `json:"has_pages"`
+	ForksCount       int         `json:"forks_count"`
+	MirrorUrl        interface{} `json:"mirror_url"`
+	Archived         bool        `json:"archived"`
+	Disabled         bool        `json:"disabled"`
+	OpenIssuesCount  int         `json:"open_issues_count"`
+	License          interface{} `json:"license"`
+	Forks            int         `json:"forks"`
+	OpenIssues       int         `json:"open_issues"`
+	Watchers         int         `json:"watchers"`
+	DefaultBranch    string      `json:"default_branch"`
+	Permissions      struct {
+		Admin bool `json:"admin"`
+		Push  bool `json:"push"`
+		Pull  bool `json:"pull"`
+	} `json:"permissions"`
+	url   string
+	stars int
+}
+
+var gitHubBaseUrl = env.Get("GITHUB_BASE_URL")
+
+// GenerateAuthenticationHeader generates the header kv used to authorize
 // our requests using the personal access token stored at .env
-func GenerateAuthenticationHeader() (key string, value string) {
+func generateAuthenticationHeader() (key string, value string) {
 	personalToken := env.Get("GITHUB_PERSONAL_ACCESS_TOKEN")
 
 	header := fmt.Sprintf("token %s", personalToken)
 
 	key = "Authentication"
 	return key, header
+}
+
+func getMostStarredRepository(repos []Repository) Repository {
+	mostStarredIndex := -1
+	stars := 0
+	for i, repo := range repos {
+		if repo.StargazersCount >= stars {
+			mostStarredIndex = i
+			stars = repo.StargazersCount
+		}
+	}
+	return repos[mostStarredIndex]
+}
+
+func generateCommonHeader() http.Header {
+	hostname, _ := os.Hostname()
+	header := http.Header{}
+	header.Add(generateAuthenticationHeader())
+	header.Add("Accept", "*/*")
+	header.Add("Connection", "keep-alive")
+	header.Add("Host", hostname)
+
+	return header
+}
+
+// generateUserFromBody creates a User from a
+// io.Reader generally provided by a http.Request
+func generateUserFromBody(requestBody io.Reader) User {
+	user := User{}
+	bodyBytes, _ := ioutil.ReadAll(requestBody)
+	err := json.Unmarshal(bodyBytes, &user)
+	if err != nil {
+		log.Print(err)
+	}
+	return user
+}
+
+// getGitHubUser get a User from GitHub API
+func getGitHubUser(user string) User {
+	formatedEndpoint := fmt.Sprintf("/users/%s", user)
+	buildedURL := gitHubBaseUrl + formatedEndpoint
+	userRequest, _ := http.NewRequest(http.MethodGet, buildedURL, nil)
+
+	userRequest.Header = generateCommonHeader()
+	client := http.Client{}
+	userResponse, err := client.Do(userRequest)
+	if err != nil {
+		log.Print(err)
+	}
+	var gitHubUser = generateUserFromBody(userResponse.Body)
+
+	return gitHubUser
 }
