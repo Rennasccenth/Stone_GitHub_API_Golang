433fb9b17a969850f58799c3170ab82ccfc369c6
Felipe Nunes <rennasccenth@gmail.com>
2021-06-01 08:16:49 -0400

    refactor: Alterna retorno de handler para melhorar a camada de teste

--- handler/handler.go
+++ handler/handler.go
@@ -17,51 +17,48 @@ func startCacheLayer() *cache.Cache {
 
 // GetUserMostStarredRepository tries to get the most starred cmd.Repository
 // on cache layer. If not found, fetches and updates the cache.
-func GetUserMostStarredRepository(userLogin string) (mostStarredRepo cmd.Repository, isCached bool) {
-	cacheKey := fmt.Sprintf("{Action_1}{%s}", userLogin)
-	isCached = false
+func GetUserMostStarredRepository(userLogin string) (mostStarredRepo cmd.Repository, cacheKey string) {
+	cacheKey = fmt.Sprintf("{Action_1}{%s}", userLogin)
 
 	cachedResponse, found := cacheLayer.Get(cacheKey)
 	if found {
 		mostStarredRepo, _ = cachedResponse.(cmd.Repository)
-		return mostStarredRepo, true
+		return mostStarredRepo, cacheKey
 	}
 	mostStarredRepo = cmd.GetUserMostStarredRepository(userLogin)
 	cacheLayer.Set(cacheKey, mostStarredRepo, cache.DefaultExpiration)
 
-	return mostStarredRepo, false
+	return mostStarredRepo, cacheKey
 }
 
 // GetMostCommentedIssue tries to get the most commented cmd.Issue
 // on cache layer. If not found , fetches and updates the cache
-func GetMostCommentedIssue(user string, repository string) (mostCommentedOpenedIssue cmd.Issue, isCached bool) {
-	cacheKey := fmt.Sprintf("{Action_2}{%s}{%s}", user, repository)
-	isCached = false
+func GetMostCommentedIssue(user string, repository string) (mostCommentedOpenedIssue cmd.Issue, cacheKey string) {
+	cacheKey = fmt.Sprintf("{Action_2}{%s}{%s}", user, repository)
 
 	cachedResponse, found := cacheLayer.Get(cacheKey)
 	if found {
 		mostCommentedOpenedIssue = cachedResponse.(cmd.Issue)
-		return mostCommentedOpenedIssue, true
+		return mostCommentedOpenedIssue, cacheKey
 	}
 	mostCommentedOpenedIssue = cmd.GetMostCommentedIssue(user, repository)
 	cacheLayer.Set(cacheKey, mostCommentedOpenedIssue, cache.DefaultExpiration)
 
-	return mostCommentedOpenedIssue, false
+	return mostCommentedOpenedIssue, cacheKey
 }
 
 // GetNonInteractedPullRequests tries to get all non interacted cmd.PullRequest
 // of a cmd.Repository on cache layer. If not found , fetches and updates the cache.
-func GetNonInteractedPullRequests(user string, repository string) (nonInteractedPullRequests []cmd.PullRequest, isCached bool) {
-	cacheKey := fmt.Sprintf("{Action_3}{%s}{%s}", user, repository)
-	isCached = false
+func GetNonInteractedPullRequests(user string, repository string) (nonInteractedPullRequests []cmd.PullRequest, cacheKey string) {
+	cacheKey = fmt.Sprintf("{Action_3}{%s}{%s}", user, repository)
 
 	cachedResponse, found := cacheLayer.Get(cacheKey)
 	if found {
 		nonInteractedPullRequests, _ = cachedResponse.([]cmd.PullRequest)
-		return nonInteractedPullRequests, true
+		return nonInteractedPullRequests, cacheKey
 	}
 	nonInteractedPullRequests = cmd.GetNonInteractedPullRequests(user, repository)
 	cacheLayer.Set(cacheKey, nonInteractedPullRequests, cache.DefaultExpiration)
 
-	return nonInteractedPullRequests, false
+	return nonInteractedPullRequests, cacheKey
 }
